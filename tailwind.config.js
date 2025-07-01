cf799872116a6b59f9a8315e61e0f163c0e761df
saltlifeshep <62799544+saltlifeshep@users.noreply.github.com>
2025-07-01 15:27:53 -0400

    chore: standardize to Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "content": [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue}",
+    "./node_modules/astro/.temp/**/*"
+  ],
+  "theme": {
+    "extend": {}
+  },
+  "plugins": [
+    "@tailwindcss/typography"
+  ]
+}
